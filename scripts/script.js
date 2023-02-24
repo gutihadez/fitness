d8085b857b9ef9f2d679d0a117c1a299ca831ee1
gutihadez <117529150+gutihadez@users.noreply.github.com>
2023-02-24 12:02:40 -0500

    update chapter 10
    update chapter 10 02/24/2023

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+/* 
+	Student Name: Agustin Hernandez
+	File Name: script.js
+	Date: 02/24/2023
+*/
+
+//Hamburger menu Function
+function hamburger() {
+	var menu = document.getElementById("menu-links");
+	var logo = document.getElementById("ffc-log");
+	if (menu.style.display === "block" && logo.style.display === "none") (
+		menu.style.display = "none";
+		logo.style.display = "block";
+	) else {
+		menu.style.display = "block";
+		logo.style.display = "none";
+	}
+}
