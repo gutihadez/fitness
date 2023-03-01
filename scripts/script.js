80a9481a1bae068c00946c4ad6ac3263c687f5a9
gutihadez <117529150+gutihadez@users.noreply.github.com>
2023-03-01 07:47:27 -0500

    update chapter 10
    update chapter 10

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,11 @@
 	Date: 02/24/2023
 */
 
+//Global variables
+var video = document.getElementById("example");
+var videoSource = document.getElementById("vid-src");
+var descriptionSource = document.getElementById("despsrc");
+
 //Hamburger menu Function
 function hamburger() {
 	var menu = document.getElementById("menu-links");
@@ -15,4 +20,36 @@ function hamburger() {
 		menu.style.display = "block";
 		logo.style.display = "none";
 	}
+}
+
+//Function to display the burpees example video
+function burpees() {
+videoSource.src = "media/burpees.mp4"
+descriptionSource.rc = "media/burpees-description.vtt";
+video.style.display = "block";
+video.load();
+}
+
+//Function to display the plank example video
+function plank() {
+videoSource.src = "media/plank.mp4"
+descriptionSource.rc = "media/plank-description.vtt";
+video.style.display = "block";
+video.load();
+}
+
+//Function to display the mountain climbers example video
+function mountain() {
+videoSource.src = "media/mc.mp4"
+descriptionSource.rc = "media/mountain-description.vtt";
+video.style.display = "block";
+video.load();
+}
+
+//Function to display a promo code
+function discount() {
+	var promo = document.getElementById("special");
+	promo.firstChild.nodeValue = "Promo Code: D25START";
+	promo.style.color = "#ff0000";
+	promo.style.fontSize = "2em";
 }
